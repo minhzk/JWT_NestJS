b124326e34f2de2e0ab55cd5391e29469607a7a9
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2025-03-03 22:47:30 +0700

    feat: get all and create user

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsNumber, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @MinLength(6)
+  password: string;
+
+  @IsNotEmpty()
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  image: string;
+}
