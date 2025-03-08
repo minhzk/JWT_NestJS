3d4c837cccabdd2ce320f10adec2f88f9cdb867f
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2025-03-08 15:30:49 +0700

    feat: transform interceptors

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './passport/local-auth.guard';
-import { Public } from '@/decorator/customize';
+import { Public, ResponseMessage } from '@/decorator/customize';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { MailerService } from '@nestjs-modules/mailer';
 
@@ -12,9 +12,10 @@ export class AuthController {
     private readonly mailerService: MailerService
   ) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Public()
   @Post("login")
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @ResponseMessage('Fetch login')
   handleLogin(@Request() req) {
     return this.authService.login(req.user)
   }
